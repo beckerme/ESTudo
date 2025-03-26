a1e125a113a8a6da028ef4c592cfd01c93aee99e
Tomas <tomas.santos@ipcbcampus.pt>
2025-03-26 22:29:18 +0000

    Tentativa 2 de commit do registo para o main

--- estudo/src/app/page.js
+++ estudo/src/app/page.js
@@ -9,18 +9,6 @@ const kanit = Kanit({
 
 export default function Home() {
   return (
-    <div>
-      <div className={`${kanit.className}`}>
-        <Image 
-          src="/logo_ESTudo.png"
-          width={100.329}
-          height={106}
-          alt="ESTudo Logo" 
-        />
-        <h1 className="text-8xl">ESTudo</h1>
-        <p className="text-xl">University Document-Sharing App</p>
-        <Input/>
-      </div>
-    </div>
+    <div></div>
   );
 }
