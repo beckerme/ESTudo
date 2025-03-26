ccfbeb2f28628db41524ad9ecadb2e0bef0a889b
Ewerton Becker <75760847+beckerme@users.noreply.github.com>
2025-03-26 23:38:51 +0000

    Mudança na fonte do placeholder do input

--- estudo/src/components/Input.jsx
+++ estudo/src/components/Input.jsx
@@ -1,9 +1,13 @@
 import { Kanit } from "next/font/google";
+const kanit = Kanit({
+    subsets: ['latin'],
+    weight: "400",
+  });
 
 export const Input = () =>{
     return <input 
-            className="bg-[#007CC2] rounded-3xl w-[20rem] h-[4rem] px-3 placeholder:text-white placeholder:Kanit"
-            placeholder="Pesquise por apontamentos" />
+            className={`bg-[#007CC2] rounded-3xl w-[49vw] h-[6vh] px-3 placeholder:text-white placeholder:${kanit.className}`}
+            placeholder="Pesquise por apontamentos"/>
 }
 
 export default Input;
