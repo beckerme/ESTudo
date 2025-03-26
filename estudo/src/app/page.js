b47da4bc9bf67b98f99b2f7d13a25293b0332028
Gongas <goncalo.c.r.santos@protonmail.com>
2025-03-26 20:24:30 +0000

    Solucionei o problema da fonte

--- estudo/src/app/page.js
+++ estudo/src/app/page.js
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import {Input} from "../components/Input";
+import "../app/globals.css";
+import { Kanit } from "next/font/google";
+
+const kanit = Kanit({
+  subsets: ['latin'],
+  weight: "400",
+});
 
 export default function Home() {
   return (
     <div className="bg-slate-50 h-screen">
-      <h1 className="fontPrincipal text-black">Login</h1>
-      <h1 className="text-black">Registo</h1>
+      <div className={`${kanit.className}`}>
+        <h1 className="text-black">Registo</h1>
+      </div>
     </div>
   );
 }
