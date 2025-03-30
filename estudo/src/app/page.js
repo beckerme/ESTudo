7ce32e62cf9448b2d687667937d232836a9d2ac9
Tomas <tomas.santos@ipcbcampus.pt>
2025-03-30 19:05:46 +0100

    Alterações relativas à conexão com o base de dados Supabase

--- estudo/src/app/page.js
+++ estudo/src/app/page.js
@@ -1,6 +1,11 @@
 import "../app/globals.css";
+import supabase from "./config/supabaseClient";
   
 export default function Home() {
+
+  //teste de ligação com o supabase
+  // console.log(supabase);
+
   return (
     <div>
     </div>
