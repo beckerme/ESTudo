8c614aa23303728aa1fb237cebb819c17512c797
cmartinsantunes <catarina.martins.antunes99@gmail.com>
2025-05-15 11:19:50 +0100

    barra de pesquisa concluida

--- estudo/src/components/BarraPesquisa.jsx
+++ estudo/src/components/BarraPesquisa.jsx
@@ -1,13 +1,19 @@
 import { Kanit } from "next/font/google";
-const kanit = Kanit({
-    subsets: ['latin'],
-    weight: "400",
-  });
 
-export const BarraPesquisa = () =>{
-    return <input 
-            className={`bg-[#007CC2] rounded-3xl w-[49vw] h-[6vh] px-3 placeholder:text-white placeholder:${kanit.className}`}
-            placeholder="Pesquise por apontamentos"/>
-}
+const kanit = Kanit({
+  subsets: ['latin'],
+  weight: "400",
+});
+
+export const BarraPesquisa = ({ onChange }) => {
+  return (
+    <input 
+      className={`bg-[#007CC2] rounded-3xl w-[49vw] h-[6vh] px-3 
+                  placeholder:text-white text-white placeholder:${kanit.className}`}
+      placeholder="Pesquise por apontamentos"
+      onChange={onChange}
+    />
+  );
+};
 
 export default BarraPesquisa;
