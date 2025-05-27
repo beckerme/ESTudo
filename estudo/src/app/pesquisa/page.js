ddd72134136af88027cfb67206a384662e0723a9
Hkmartimxp6 <martimmartins20@gmail.com>
2025-05-27 10:23:40 +0100

    pesquisa
    protecao

--- estudo/src/app/pesquisa/page.js
+++ estudo/src/app/pesquisa/page.js
@@ -1,5 +1,31 @@
+"use client";
+
 import Pesquisa from "@/components/Pesquisa/Pesquisa";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function App() {
+
+const router = useRouter();
+const [autorizado, setAutorizado] = useState(null); // null = verificando
+
+  useEffect(() => {
+    const tipo = localStorage.getItem("tipoUsuario");
+
+    if (tipo === "admin") {
+      setAutorizado(false);
+      router.push("/mensagem-erro");
+    } else {
+      setAutorizado(true);   
+    }
+  }, [router]);
+
+  if (autorizado === null) {
+    return null; 
+  }
+
+  if (!autorizado) {
+    return null; 
+  }
     return <Pesquisa />;
 }
